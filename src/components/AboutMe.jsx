f432b37e8311d3c8cfd30f7412e896571dec81ae
Terrence David <terrdv@students.cs.ubc.ca>
2025-07-25 20:26:53 -0400

    website

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,21 @@
+import '../css/AboutMe.css'
+
+function AboutMe() {
+    return (
+        <div className="abt">
+            <div className="head">
+                <img className="pic" src="/images/td.jpeg" alt="Terrence David Headshot"/>
+                <h2>Terrence David</h2>
+            </div>
+            <div className="text">
+                <h2 className="tdvheader">
+                    About Me
+                </h2>
+                <p>Hey, I'm Terrence David! I am a second year Computer Science and Statistics student at <bold>The University of British Columbia</bold> . I'm interested in web development, software development and machine learning. My best programming languages are Python, Java and JavaScript!
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default AboutMe
